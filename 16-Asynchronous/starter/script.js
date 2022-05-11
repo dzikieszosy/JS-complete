156eaf78383437db49fd78a85a2d97cc57d87f51
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2022-05-11 06:00:01 +0200

    forkify project started

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,6 +1,85 @@
-'use strict';
-
-const btn = document.querySelector('.btn-country');
+/*
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    // console.log(data); //use this to study the data you want to use.
+
+    const languages = Object.values(data.languages);
+    const currencies = Object.values(data.currencies);
+    const html = `
+  <article class="country">
+    <img class="country__img" src= "${data.flags.svg}">
+    <div class="country__data">
+      <h3 class="country__name"> ${data.name.official}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        data.population / 1000000
+      ).toFixed(1)} million</p>         
+      <p class="country__row"><span>🗣️</span>${languages[0]}</p>
+      <p class="country__row"><span>💰</span>${currencies[0].name}</p>
+    </div>
+  </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+// Sample countries whose details we want to display.
+getCountryData('japan');
+getCountryData('australia');
+getCountryData('india');
+getCountryData('usa');
+*/
+
+// Loading images in seconds
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('image not found'));
+    });
+  });
+};
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
